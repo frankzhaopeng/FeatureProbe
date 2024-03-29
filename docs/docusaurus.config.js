c47e5acddefb9821bdbf2c3d102f16bc8e1c0568
poryzhao <poryzhao@gmail.com>
2024-03-29 15:42:08 +0800

    Update docusaurus.config.js

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -20,8 +20,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'FeatureProbe', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
+  organizationName: 'frankzhaopeng', // Usually your GitHub org/user name.
+  projectName: 'FeatureProbe', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
